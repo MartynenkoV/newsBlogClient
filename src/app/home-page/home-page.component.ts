7ecf649e29273a05004a2c78fdb60f48505f7a23
Vova <renarblackfox@gmail.com>
2017-01-05 19:02:33 +0200

    template

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  data = [{
+  	title:"Title1",
+  	content:"aaaaaaaa",
+  	date: "dd/MM/yyyy",
+  	comments:[1,2,3,4,5,6],
+  	image:"../assets/hierarchicalstructureofthei.jpg"
+  },{
+  	title:"Title2",
+  	content:"aaaaaaaa",
+  	date: "dd/MM/yyyy",
+  	comments:[1,2,3,4,6],
+  	image:"../assets/fc7021_61c6672c854f4e41ba9ad1e1783ace3a.jpg"
+  },{
+  	title:"Title3",
+  	content:"aaaaaaaa",
+  	date: "dd/MM/yyyy",
+  	comments:[1,2,3,4,5,6],
+  	image:"../assets/looksdelicious.jpg"
+  },{
+  	title:"Title4",
+  	content:"aaaaaaaa",
+  	date: "dd/MM/yyyy",
+  	comments:[1,2,3],
+  	image:"../assets/mappingtheblogosphere.jpg"
+  }]	
   constructor() { }
 
   ngOnInit() {
--- src/app/news-preview/news-preview.component.ts
+++ src/app/news-preview/news-preview.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-news-preview',
   templateUrl: './news-preview.component.html',
-  styleUrls: ['./news-preview.component.css']
+  styleUrls: ['./news-preview.component.css'],
+
 })
 export class NewsPreviewComponent implements OnInit {
+  
+  @Input() data:any; 	
+
+  // data = {
+  // 	title:"Title1",
+  // 	content:"aaaaaaaa",
+  // 	date: "dd/MM/yyyy",
+  // 	comments:[1,2,3,4,5,6],
+  // 	image:"../assets/hierarchicalstructureofthei.jpg"
+  // }
 
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
